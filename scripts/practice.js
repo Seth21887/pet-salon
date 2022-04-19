aaf0642bb3f2121b04573da9216750ce3c175706
Seth21887 <101611145+Seth21887@users.noreply.github.com>
2022-04-18 19:48:51 -0700

    after class 2

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,49 +1,49 @@
 
 //objects literal use {}
-let student1 = {
-    //attr:value,
-    firstName:"Astrid",
-    lastName:"Batres-Guerrero",
-    age:25,
-    isActive:true
-}
+// let student1 = {
+//     //attr:value,
+//     firstName:"Astrid",
+//     lastName:"Batres-Guerrero",
+//     age:25,
+//     isActive:true
+// }
 
-let student2 = {
-    firstName:"Seth",
-    lastName:"LaFountain",
-    age:22,
-    isActive:true
-}
-let student3 = {
-    firstName:"Ben",
-    lastName:"Vance",
-    age:25,
-    isActive:false,
-    hobbies:["listen to music", "read", "play zelda"],
-    address:{
-        street: "Palm Street",
-        zip: "22796",
-        number:"262-k"
-    }
-}
+// let student2 = {
+//     firstName:"Seth",
+//     lastName:"LaFountain",
+//     age:22,
+//     isActive:true
+// }
+// let student3 = {
+//     firstName:"Ben",
+//     lastName:"Vance",
+//     age:25,
+//     isActive:false,
+//     hobbies:["listen to music", "read", "play zelda"],
+//     address:{
+//         street: "Palm Street",
+//         zip: "22796",
+//         number:"262-k"
+//     }
+// }
 
-console.log(student1.firstName);
-console.log(student2['lastName']);
-console.log(student3.hobbies[1]); //display read
-console.log(student3.address.zip); //display zip
-console.log(student3["address"]["zip"]);
+// console.log(student1.firstName);
+// console.log(student2['lastName']);
+// console.log(student3.hobbies[1]); //display read
+// console.log(student3.address.zip); //display zip
+// console.log(student3["address"]["zip"]);
 
-//challenge: create an object for a client with the attributes first name, last name, subscription type, active or not, credits
+// //challenge: create an object for a client with the attributes first name, last name, subscription type, active or not, credits
 
-let client1 = {
-    firstName:"Joseph",
-    lastName:"Josephson",
-    subType:"Annual",
-    isActive:true,
-    credits:24.37
-}
+// let client1 = {
+//     firstName:"Joseph",
+//     lastName:"Josephson",
+//     subType:"Annual",
+//     isActive:true,
+//     credits:24.37
+// }
 
-console.log(client1);
+// console.log(client1);
 
 // let students=[{
 //     //attr:value,
@@ -73,18 +73,46 @@ console.log(client1);
 
 //object constructor
 
-function displayStudent(){
-    let tmp=""
-    for(let i=0;i<students.length;i++){
-        tmp+=`<li>${students[i].firstName}</li>`;
-        console.log(tmp);
-
-        document.getElementById("students").innerHTML=tmp;
-        
-    }
+//these are local variables, they are only available here.
+function Students(fname,lname,age,isActive){
+    this.firstName=fname;
+    this.lastName=lname;
+    this.age=age;
+    this.isActive=isActive;
 }
 
-displayStudent();
+let student1 = new Students("Astrid","Bates-Guerrero",25,true);
+let student2 = new Students("Alexis","Aldrete",25,true);
+
+console.log(student1,student2);
+
+//Exercise:
+//Create an object constructor function and two objects
+function BasketballPlayers(fname,lname,position, jerseyNumber, team){
+    this.firstName=fname;
+    this.lastName=lname;
+    this.position=position;
+    this.jerseyNumber=jerseyNumber;
+    this.team=team;
+}
+
+let player1 = new BasketballPlayers("Kobe","Bryant","SG",8,"Lakers");
+let player2 = new BasketballPlayers("Lebron","James","SF",6,"Lakers");
+
+console.log(player1,player2);
+
+// function displayStudent(){
+//     let tmp=""
+//     for(let i=0;i<students.length;i++){
+//         tmp+=`<li>${students[i].firstName}</li>`;
+//         console.log(tmp);
+
+//         document.getElementById("students").innerHTML=tmp;
+        
+//     }
+// }
+
+//displayStudent();
 
 // let p = document.createElement("p");
 // console.log(p);
--- scripts/register.js
+++ scripts/register.js
@@ -8,45 +8,89 @@ let petSalon = {
     hours:{
         open:"9:00 a.m.",
         close:"8:00 p.m."
-    }
+    },
+    pets:[]
 }
-let pets =[{
-            name:"Scooby",
-            age:50,
-            breed:"Dane",
-            gender:"Male",
-            service:"Grooming",
-            ownersName:"Shaggy",
-            contactPhone:"555-555-5555"
-        },
-        {
-            name:"Scrappy",
-            age:40,
-            breed:"Mixed",
-            gender:"Male",
-            service:"Grooming",
-            ownersName:"Shaggy",
-            contactPhone:"555-555-5555"
-        },
-        {
-            name:"Bowser",
-            age:12,
-            breed:"English Bulldog",
-            gender:"Male",
-            service:"Grooming",
-            ownersName:"Seth",
-            contactPhone:"619-555-4444"
-        },
-        {
-            name:"Tommy Pickles",
-            age:6,
-            breed:"Pug",
-            gender:"Male",
-            service:"Grooming",
-            ownersName:"Seth",
-            contactPhone:"619-555-4444"
-        }
-    ];
+//name,age,gender,breed,service,owner name, contact phone
+function Pet(name,age,breed,gender,service,ownersName,contactPhone){
+    this.name=name;
+    this.age=age;
+    this.breed=breed;
+    this.gender=gender;
+    this.service=service;
+    this.ownersName=ownersName;
+    this.contactPhone=contactPhone;
+}
+
+//get the info from the inputs and store the information
+let inputName=document.getElementById("txtName");
+let inputAge=document.getElementById("txtAge");
+let inputGender=document.getElementById("txtGender");
+let inputBreed=document.getElementById("txtBreed");
+let inputService=document.getElementById("selService");
+let inputOwner=document.getElementById("txtOwner");
+let inputPhone=document.getElementById("txtPhone");
+
+function register(){
+    //create the pet
+    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputOwner.value,inputPhone.value);
+    console.log(thePet);
+    //push the pet into the array
+    petSalon.pets.push(thePet);
+    //clear the inputs
+    clearInputs();
+}
+
+function clearInputs(){
+
+}
+
+let scooby = new Pet("Scooby",50,"Dane","Male","Grooming","Shaggy","555-555-5555");
+let scrappy = new Pet("Scrappy",40,"Mixed","Male","Grooming","Shaggy","555-555-5555");
+let bowser = new Pet("Bowser", 12, "English Bulldog", "Male", "Grooming", "Seth", "619-555-4444");
+let tommyPickles = new Pet("Tommy Pickles",6,"Pug","Male","Grooming","Seth","619-555-4444");
+
+console.log(scooby,scrappy,bowser,tommyPickles);
+
+//create 3 pets
+
+// let pets =[{
+//             name:"Scooby",
+//             age:50,
+//             breed:"Dane",
+//             gender:"Male",
+//             service:"Grooming",
+//             ownersName:"Shaggy",
+//             contactPhone:"555-555-5555"
+//         },
+//         {
+//             name:"Scrappy",
+//             age:40,
+//             breed:"Mixed",
+//             gender:"Male",
+//             service:"Grooming",
+//             ownersName:"Shaggy",
+//             contactPhone:"555-555-5555"
+//         },
+//         {
+//             name:"Bowser",
+//             age:12,
+//             breed:"English Bulldog",
+//             gender:"Male",
+//             service:"Grooming",
+//             ownersName:"Seth",
+//             contactPhone:"619-555-4444"
+//         },
+//         {
+//             name:"Tommy Pickles",
+//             age:6,
+//             breed:"Pug",
+//             gender:"Male",
+//             service:"Grooming",
+//             ownersName:"Seth",
+//             contactPhone:"619-555-4444"
+//         }
+//     ];
 
 console.log(petSalon);
 
